6abd32d712ce482600d74df94aa94018e5044cc7
Bryan-Farinango <38628690+Bryan-Farinango@users.noreply.github.com>
2018-07-16 23:14:30 -0500

    Add files via upload

---
+++ Datos/src/DatosDeber/Materias.java
@@ -0,0 +1,29 @@
+
+package DatosDeber;
+
+
+public class Materias {
+    
+    private String nombre;
+
+    public Materias(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre;
+    }
+    
+    
+    
+}
